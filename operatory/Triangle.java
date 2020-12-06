ee0f7b1fe704e39cc680087b278a7d2138c20808
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-06 13:24:06 +0100

    #9 Obwód trójkąta

---
+++ operatory/Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        int c = 12;
+        int triangleRegion;
+        triangleRegion = a+b+c;
+        System.out.println("Obwód trójkąta: "+triangleRegion);
+    }
+}
