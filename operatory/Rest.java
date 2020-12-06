1c4397704415b41a00acf225aaea9fb35a349fa5
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-06 13:45:43 +0100

    #11 obliczenie reszty z dzielenia

---
+++ operatory/Rest.java
@@ -0,0 +1,15 @@
+public class Rest {
+    public static void main(String[] args) {
+        int a = 47;
+        int b = 10;
+        double c = 35.5;
+        int d = 6;
+        int e;
+        double f;
+        e = a%b;
+        f = c%d;
+        System.out.println("Obliczenie wartości modulo; :"+e);
+        System.out.println("Obliczenie wartości modulo; :"+f);
+
+    }
+}
