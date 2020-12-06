2a398cd30a18791cb55e2bb1ff4683fa9ca1c2f0
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-06 12:59:47 +0100

    #5 program wyswietlajacy dane podrozy

---
+++ operatory/Travel.java
@@ -0,0 +1,13 @@
+public class Travel {
+    public static void main(String[] args) {
+        int travelDistance = 300;
+        int travelTimeInMinutes  = 157;
+        byte travelDays = 1;
+        double accountBalance = 1345.74;
+
+        System.out.println("dystans podróży: "+travelDistance);
+        System.out.println("czas podróży: "+travelTimeInMinutes);
+        System.out.println("podróż w dniach: "+travelDays);
+        System.out.println("stan konta po podróży: " + accountBalance);
+    }
+}
